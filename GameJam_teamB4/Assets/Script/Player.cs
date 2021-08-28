f9afe8aa655deb11a01d850e6498e6f7dc7bfd08
Ws-Peroth <a01033056809@gmail.com>
2021-08-28 15:25:27 +0900

    ADD Player Anim

--- GameJam_teamB4/Assets/Script/Player.cs
+++ GameJam_teamB4/Assets/Script/Player.cs
@@ -5,20 +5,77 @@ using UnityEngine.UI;
 
 namespace lws
 {
-    public class Player : MonoBehaviour
+    public class Player : PlayerMove
     {
         [SerializeField] Slider hpBar;
         public float hp = 100;
+        [SerializeField] private bool isAttack;
+        [SerializeField] private bool isSkill;
 
-        void Start()
+        public override void Start()
         {
+            base.Start();
 
+            hp = 100;
+            isAttack = false;
+            isSkill = false;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void Update()
         {
+            if (!isSkill) Attack();
+            if (!isAttack) Skill();
+
+            GroundCheck();
+
+            if (!isAttack && !isSkill)
+            {
+                Move();
+                Jump();
+            }
+
             hpBar.value = hp;
+        }
+
+        void Attack()
+        {
+            if (!isAttack && Input.GetKey(KeyCode.Z))
+            {
+                isAttack = true;
+
+                playerAnimator.SetBool("isAttack", true);
+            }
+            if (isAttack)
+            {
+                // 기존의 힘을 유지
+                if (isGround) playerRigidbody.velocity = Vector2.zero;
+            }
+        }
+
+        void Skill()
+        {
+            if (!isSkill && Input.GetKey(KeyCode.X))
+            {
+                isSkill = true;
+
+                playerAnimator.SetBool("isSkill", true);
+            }
+            if (isSkill)
+            {
+                // 기존의 힘을 유지
+                if (isGround) playerRigidbody.velocity = Vector2.zero;
+            }
+        }
+
+        public void AttackEnd()
+        {
+            isAttack = false;
+            playerAnimator.SetBool("isAttack", false);
+        }
+        public void SkillEnd()
+        {
+            isSkill = false;
+            playerAnimator.SetBool("isSkill", false);
         }
     }
 }
--- GameJam_teamB4/Assets/Script/PlayerMove.cs
+++ GameJam_teamB4/Assets/Script/PlayerMove.cs
@@ -9,36 +9,30 @@ namespace lws
         public Animator playerAnimator;
         public Rigidbody2D playerRigidbody;
         public SpriteRenderer playerSpriteRenderer;
+        public bool isGround = true;
 
         [SerializeField] private float speed;
         [SerializeField] private float jumpPower;
         [SerializeField] private float downSpeed;
-        [SerializeField] private bool isGround = true;
 
         // Start is called before the first frame update
-        void Start()
+        public virtual void Start()
         {
-
             speed = 10;
-            jumpPower = 400;
+            jumpPower = 600;
             downSpeed = 3f;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void GroundCheck()
         {
-            Move();
-            Jump();
-        }
-
-        void Jump()
-        {
-            playerRigidbody.gravityScale = downSpeed;
-
             // ↑ 점프, 바닥체크
             isGround = Physics2D.OverlapCircle((Vector2)transform.position
                 + new Vector2(0, -0.9f), 0.07f, 1 << LayerMask.NameToLayer("Ground"));
+        }
 
+        public void Jump()
+        {
+            playerRigidbody.gravityScale = downSpeed;
 
             if (Input.GetKeyDown(KeyCode.UpArrow) && isGround)
             {
@@ -49,7 +43,7 @@ namespace lws
             }
         }
 
-        private void Move()
+        public void Move()
         {
             if (Input.GetKey(KeyCode.RightArrow))
             {
