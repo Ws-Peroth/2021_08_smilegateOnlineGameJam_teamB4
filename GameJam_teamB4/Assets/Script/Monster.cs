b9f739f1c2a61d019bd23875175d57edc3821df0
Song nari <na061300@naver.com>
2021-08-28 23:39:29 +0900

    상어 몬스터 추가

--- GameJam_teamB4/Assets/Script/Monster.cs
+++ GameJam_teamB4/Assets/Script/Monster.cs
@@ -29,7 +29,7 @@ public class Monster : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        checkFront();
+        //checkFront();
 
         if (!attack)
         {
